5346f163a0f66f701ec390e56c4c3c4bd4a8fc41
ctrlKshav <keshavm9978@gmail.com>
2025-02-03 17:26:55 +0530

    supabase

---
+++ src/utils/supabase.ts
@@ -0,0 +1,8 @@
+﻿
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl ? supabaseUrl : "", supabaseKey ? supabaseKey : "");
+
+export default supabase
